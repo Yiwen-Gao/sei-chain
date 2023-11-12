5eb5a74252b5a10162e9b908e154ec9c0afb816a
Yiwen Gao <yiwen.gao@columbia.edu>
2023-11-12 14:50:16 -0800

    Add validation for Nitro txs

--- x/nitro/client/cli/tx.go
+++ x/nitro/client/cli/tx.go
@@ -32,30 +32,38 @@ func GetTxCmd() *cobra.Command {
 
 func NewRecordTransactionDataCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "record-txs [slot] [root] [tx1,tx2...]",
+		Use:   "record-txs [slot] [root] [tx1 tx2 ...]",
 		Short: "record nitro transactions and state root for a slot",
 		Args:  cobra.MinimumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
-				return err
+				return fmt.Errorf("unable to get context: %w", err)
 			}
 
 			slot, err := strconv.ParseUint(args[0], 10, 64)
 			if err != nil {
-				return nil
+				return fmt.Errorf("unable to parse slot: %w", err)
 			}
+
+			root := args[1]
+			if !isValidBlockHash(root) {
+				return fmt.Errorf("invalid state root format")
+			}
+
 			txs := []string{}
 			for i := 2; i < len(args); i++ {
+				if !isValidHex(args[i]) {
+					return fmt.Errorf("transaction data needs to be hex")
+				}
 				txs = append(txs, args[i])
 			}
 
 			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags()).WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
-
 			msg := types.NewMsgRecordTransactionData(
 				clientCtx.GetFromAddress().String(),
 				slot,
-				args[1],
+				root,
 				txs,
 			)
 
@@ -65,4 +73,14 @@ func NewRecordTransactionDataCmd() *cobra.Command {
 
 	flags.AddTxFlagsToCmd(cmd)
 	return cmd
+}
+
+func isValidHex(input string) bool {
+	_, err := strconv.ParseInt(input, 16, 64)
+	return err == nil
+}
+
+func isValidBlockHash(input string) bool {
+	// a block hash is a 64 character hex string
+	return isValidHex(input) && len(input) == 64
 }
